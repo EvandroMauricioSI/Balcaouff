34820c462be21c5fd8fab8d0bc1ad1dbc8358f1b
Laryssa Paiva <laryssapaiva@id.uff.br>
2024-11-26 19:09:55 -0300

    add model

--- backend/app.py
+++ backend/app.py
@@ -12,7 +12,7 @@ load_dotenv()
 # namespaces das rotas
 from routes.hello_routes import hello_ns
 
-# models
+# # models
 from models import usuario_model
 
 api = Api()
@@ -20,22 +20,13 @@ api = Api()
 # db = SQLAlchemy()
 app = Flask(__name__)
 
+print("aaaaaaaaaa", os.getenv('DATABASE_URL'))
 app.config['SQLALCHEMY_DATABASE_URI'] = os.getenv('DATABASE_URL')
 
 api.add_namespace(hello_ns)
 
 api.init_app(app)
 db.init_app(app)
-
-# class User(db.Model):
-#     __tablename__ = 'users'
-
-#     id = db.Column(db.Integer, primary_key=True)
-#     username = db.Column(db.String(80), unique=True, nullable=False)
-#     email = db.Column(db.String(120), unique=True, nullable=False)
-
-#     def json(self):
-#         return {'id': self.id,'username': self.username, 'email': self.email}
 
 try:
     with app.app_context():
