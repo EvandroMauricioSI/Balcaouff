aeeebdbc55356667a8fd9a688f277d79f25d8031
Laryssa Machado Rabelo de Paiva <laryssa.paiva@green4t.com>
2024-11-26 14:46:30 -0300

    testando importacoes demodulos

--- backend/app.py
+++ backend/app.py
@@ -3,64 +3,39 @@ from dotenv import load_dotenv
 
 from flask import Flask
 from flask_restx import Api
-from sqlalchemy import create_engine
-# from extensions import db
 from flask_sqlalchemy import SQLAlchemy
+
+from extensions import db
 
 load_dotenv()
 
 # namespaces das rotas
-# from routes.hello_routes import hello_ns
+from routes.hello_routes import hello_ns
 
+# models
+from models import usuario_model
 
-# def create_app():
-#     app = Flask(__name__)
+api = Api()
 
-#     print("POSTGRES_USER:", os.getenv("POSTGRES_USER"))
-#     print("POSTGRES_PASSWORD:", os.getenv("POSTGRES_PASSWORD"))
-#     print("POSTGRES_HOST:", os.getenv("POSTGRES_HOST"))
-#     print("POSTGRES_PORT:", os.getenv("POSTGRES_PORT"))
-#     print("POSTGRES_DB:", os.getenv("POSTGRES_DB"))
-#     print("DATABASE_URL:", os.getenv("DATABASE_URL"))
-
-#     # configure_database(app) 
-#     # app.config['SQLALCHEMY_DATABASE_URI'] = os.getenv('DATABASE_URL')
-#     app.config['SQLALCHEMY_DATABASE_URI'] = "postgresql://psql:senha@localhost:5432/psqldb"
-#     db = SQLAlchemy(app)
-
-#     api.init_app(app)
-
-#     # adiciona namespaces 
-#     api.add_namespace(hello_ns)
-
-#     return app, db
-
-
-# api = Api()
-# app, db = create_app()
-
-# try:
-#     with app.app_context():
-#         db.create_all()
-#     print("Tabelas criadas com sucesso!")
-# except Exception as e:
-#     print(f"Erro ao criar as tabelas: {e}")
-
-db = SQLAlchemy()
+# db = SQLAlchemy()
 app = Flask(__name__)
-print("aaaaaaaaaaaaaaaaaa", os.getenv('DATABASE_URL'))
+
 app.config['SQLALCHEMY_DATABASE_URI'] = os.getenv('DATABASE_URL')
+
+api.add_namespace(hello_ns)
+
+api.init_app(app)
 db.init_app(app)
 
-class User(db.Model):
-    __tablename__ = 'users'
+# class User(db.Model):
+#     __tablename__ = 'users'
 
-    id = db.Column(db.Integer, primary_key=True)
-    username = db.Column(db.String(80), unique=True, nullable=False)
-    email = db.Column(db.String(120), unique=True, nullable=False)
+#     id = db.Column(db.Integer, primary_key=True)
+#     username = db.Column(db.String(80), unique=True, nullable=False)
+#     email = db.Column(db.String(120), unique=True, nullable=False)
 
-    def json(self):
-        return {'id': self.id,'username': self.username, 'email': self.email}
+#     def json(self):
+#         return {'id': self.id,'username': self.username, 'email': self.email}
 
 try:
     with app.app_context():
--- backend/models/usuario_model.py
+++ backend/models/usuario_model.py
@@ -1,5 +1,5 @@
-# from ..extensions import db
-from ..app import db
+from extensions import db
+# from ..app import db
 
 class Usuario(db.Model):
     __tablename__ = 'usuarios'
