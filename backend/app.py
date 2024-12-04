40a112310a3f7d8275712622c66f4ac64273f22b
Claralino <claralino@id.uff.br>
2024-12-04 18:57:23 -0300

    adiciona imports no app.py

--- backend/app.py
+++ backend/app.py
@@ -1,12 +1,23 @@
 import os
 from dotenv import load_dotenv
-
+from extensions import db
 from flask import Flask
 from flask_restx import Api
 from flask_sqlalchemy import SQLAlchemy
 
 # namespaces das rotas
 from routes.hello_routes import hello_ns
+from routes.anuncios_routes import anuncios_ns
+from routes.categorias_routes import categorias_ns
+from routes.localizacao_routes import localizacao_ns
+from routes.usuarios_routes import usuarios_ns
+
+# adicionando namespaces
+api.add_namespace(hello_ns)
+api.add_namespace(usuarios_ns)
+api.add_namespace(anuncios_ns)
+api.add_namespace(localizacao_ns)
+api.add_namespace(categorias_ns)
 
 # Importar todos os models
 from models.anuncios_model import Anuncio
@@ -14,16 +25,8 @@ from models.usuario_model import Usuario
 from models.localizacao_model import Localizacao
 from models.categorias_model import Categoria
 
-from extensions import db
 
 load_dotenv()
-
-# namespaces das rotas
-from routes.hello_routes import hello_ns
-from routes.usuarios_routes import usuarios_ns
-
-# models
-from models import usuario_model
 
 api = Api()
 
@@ -31,9 +34,7 @@ app = Flask(__name__)
 
 app.config["SQLALCHEMY_DATABASE_URI"] = os.getenv("DATABASE_URL")
 
-# adicionando namespaces
-api.add_namespace(hello_ns)
-api.add_namespace(usuarios_ns)
+
 
 api.init_app(app)
 db.init_app(app)
